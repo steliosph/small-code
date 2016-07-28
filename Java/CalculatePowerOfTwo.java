f27b5c7cf7dbb10d268ee5b3d47b9d533d991ab2
Stelios Philippou <stevoo82@gmail.com>
2016-07-28 14:52:39 +0300

    Create CalculatePowerOfTwo.java

---
+++ Java/CalculatePowerOfTwo.java
@@ -0,0 +1,41 @@
+public class CalculatePowerOfTwo  {
+
+	// Write function to calculate sum of first N powers of 2 starting from 1.
+	// You shouldn't use any built-in function for calculating power.
+	// Implement the most efficient solution.
+
+	public static void main(String[] args) {
+		TestExample example = new TestExample();
+	}
+
+	private long executionTime;
+
+	private CalculatePowerOfTwo() {
+		startStopWatch();
+		calculateSum(21);
+		getElapsedTime();
+
+	}
+
+	public long calculateSum(int times) {
+		// The most efficient way is to calculate 2^n+1−2
+		long result = 2;
+		for (int i = 1; i <= times; i++) {
+			result *= 2;
+		}
+		long answer = result = result = 2;
+		System.out.println("calculateSum:" + answer);
+
+		return answer;
+	}
+
+	private void startStopWatch() {
+		executionTime = System.currentTimeMillis();
+	}
+
+	public void getElapsedTime() {
+		long endTime = System.currentTimeMillis();
+		System.out.println("time:" + (double) (endTime - executionTime) / (1000));
+	}
+
+}
